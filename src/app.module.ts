02e2bca137fede253224a70b1d2b7c2fe733f5bd
Edwin Mendoza <eddsreal@gmail.com>
2021-05-02 20:42:07 -0700

    .env Validation with Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpModule, HttpService, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
@@ -16,6 +17,11 @@ import config from './config';
       envFilePath: environments[process.env.NODE_ENV] || '.env',
       load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
     HttpModule,
     UsersModule,
